103774faa91935d47a975d0645ef64e50d323c9b
aryan <aryansundar22@gmail.com>
2025-12-31 21:44:49 +0530

    Add files via upload

---
+++ addition of two numbers.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a,b;
+	clrscr();
+	printf("\n enter first number");
+	scanf("%d",&a);
+	printf("\n enter second number");
+	scanf("%d",&b);
+	a=a+b;
+	printf("\n addition of two number=%d",a);
+	getch();
+}
