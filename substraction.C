467c1e6d6c9eb30eac47e1c641236b30e24f81f6
aryan <aryansundar22@gmail.com>
2026-01-02 10:26:30 +0530

    Add files via upload

---
+++ substraction.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a,b,sub;
+	clrscr();
+	printf("\n enter first number");
+	scanf("%d",&a);
+	printf("\n enter second number");
+	scanf("%d",&b);
+	sub=a-b;
+	printf("\n subtraction of two numbers=%d",sub);
+	getch();
+}
