72cfa706aab3f35822a6703c7652c83c0456b95c
aryan <aryansundar22@gmail.com>
2026-01-03 10:15:08 +0530

    Add files via upload

---
+++ MUL.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a,b,mul;
+	clrscr();
+	printf("\n enter number a");
+	scanf("%d",&a);
+	printf("\n enter number b");
+	scanf("%d",&b);
+	mul=a*b;
+	printf("\n multiplication is =%d",mul);
+	getch();
+}
